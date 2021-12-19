786d12eb5c44670d4e56450c5d1258da4e2812f6
NikolaSedProgger <forniteturbokosh@gmail.com>
2021-12-19 14:39:45 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import requests
+import urllib
+import os
+
+os.makedirs("books")
+
+
+for id in range(10):
+    url = f"http://tululu.org/txt.php?id={id+1}"
+    response = requests.get(url)
+    response.raise_for_status() 
+    filename = f'id{id+1}.txt'
+    if '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"' not in response.text:
+        with open(f'books/{filename}', 'w') as file:
+            file.write(response.text)
