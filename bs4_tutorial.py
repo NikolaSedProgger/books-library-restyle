ea819071d45abcce74b7360ee0ea92745cc25b59
NikolaSedProgger <forniteturbokosh@gmail.com>
2022-01-29 15:23:35 +0300

    download books with titles

---
+++ bs4_tutorial.py
@@ -0,0 +1,33 @@
+from tkinter.messagebox import NO
+from bs4 import BeautifulSoup
+import requests
+from pathvalidate import sanitize_filename
+from main import check_for_redirect
+
+
+def get_post(book_id):
+    url = f'http://tululu.org/b{book_id}/'
+    response = requests.get(url)
+    response.raise_for_status()
+    
+    if check_for_redirect(response):
+        soup = BeautifulSoup(response.text, 'lxml')
+
+        post_image = f"https://tululu.org/{soup.find('table').find('td', class_='ow_px_td').find('img')['src']}"
+        post_title = soup.find('table').find('h1').text.replace('::', '').split('      ')
+        post_text = soup.find('table').find('div', id='content').find_all('table', class_='d_book')[1].find('td').text
+        return post_image, post_title, post_text
+
+def download_txt(url, filename, folder='books/'):
+    response = requests.get(url, allow_redirects=True)
+    response.raise_for_status()
+    if check_for_redirect(response):
+        with open(f'{folder + sanitize_filename(filename)}', 'w') as file:
+            file.write(response.text)
+
+for book_id in range(1, 11):
+    url = f"http://tululu.org/txt.php?id={book_id}"
+    try:
+        download_txt(url, get_post(book_id)[1][0])
+    except:
+        None
--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import requests
-import urllib
 import os
 
+
+
 os.makedirs("books", exist_ok=True)
+def check_for_redirect(response):
+    if not response.history:
+        return True
+        
+
 
 
 for id in range(10):
     url = f"http://tululu.org/txt.php?id={id+1}"
-    response = requests.get(url)
+    response = requests.get(url, allow_redirects=True)
     response.raise_for_status() 
     filename = f'id{id+1}.txt'
-    if '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"' not in response.text:
+    if check_for_redirect(response):
         with open(f'books/{filename}', 'w') as file:
             file.write(response.text)
