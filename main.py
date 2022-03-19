405a7b5c88367999fff149e7afb70995c70aab22
NikolaSedProgger <forniteturbokosh@gmail.com>
2022-03-19 15:36:55 +0300

    Сделал код надёжнее

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
+from tkinter.messagebox import NO
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 from pathvalidate import sanitize_filename
 import requests
+from requests import HTTPError
 import os
 import argparse
 
+
 def check_for_redirect(response):
-    if not response.history:
-        return True
+    if response.history:
+        raise HTTPError
+
 
 def parse_book_page(response):
     soup = BeautifulSoup(response.text, 'lxml').find('table')
-
-
-
     book_image = f"https://tululu.org/{soup.find('td', class_='ow_px_td').find('img')['src']}"
     book_title = soup.find('h1').text.replace('::', '').split('      ')
     book_author = book_title[1]
@@ -30,26 +31,27 @@ def parse_book_page(response):
     }
     return parsed_book
 
-def download_text(url, filename, folder):
+
+def download_text(url, filename):
     response = requests.get(url, allow_redirects=True)
     response.raise_for_status()
     if check_for_redirect(response):
-        with open(f'{folder}{sanitize_filename(filename)}.txt', 'w') as file:
+        with open(f'books/{sanitize_filename(filename)}.txt', 'w') as file:
             file.write(response.text)
 
-def download_image(url, folder):
+
+def download_image(url):
     response = requests.get(url, allow_redirects=True)
     response.raise_for_status()
     if check_for_redirect(response):
         filename = urlparse(url).path.replace("//images/", "")
-        with open(f'{folder}{sanitize_filename(filename)}', 'wb') as file:
-            file.write(response.content) 
+        with open(f'images/{sanitize_filename(filename)}', 'wb') as file:
+            file.write(response.content)
 
 
 if __name__ == '__main__':
     os.makedirs("books", exist_ok=True)
     os.makedirs("images", exist_ok=True)
-    
     parser = argparse.ArgumentParser(
         description='Программа скачивает книги по их id с tululu.org'
     )
@@ -57,12 +59,15 @@ if __name__ == '__main__':
     parser.add_argument('end_id', help='Последний id книги', type=int)
     args = parser.parse_args()
 
-    for book_id in range(args.start_id, args.end_id):
-        url = f'http://tululu.org/b{book_id}/'
-        response = requests.get(url)
+    for book_id in range(args.start_id, args.end_id+1):
+        url = f'https://tululu.org/b{book_id}/'
+        response = requests.get(url, allow_redirects=True)
         response.raise_for_status()
-        if check_for_redirect(response):
+        try:
+            check_for_redirect(response)
             parsed_book = parse_book_page(response)
             print(parsed_book)
-            download_text(parsed_book['book_text'])
+            download_text(f'https://tululu.org/txt.php?id={book_id}', parsed_book['book_title'])
             download_image(parsed_book['book_image'])
+        except HTTPError:
+            print(response.history)
